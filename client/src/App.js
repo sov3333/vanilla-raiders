b59b935d11da6a52169280873fa7e6930c0a137e
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-12-16 17:46:36 +0800

    setup arena page skeleton

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import SelectCharacter from './Components/SelectCharacter';
 import { CONTRACT_ADDRESS, transformCharacterData } from './constants';
 import myEpicGame from './utils/MyEpicGame.json';
 import { ethers } from 'ethers';
+import Arena from './Components/Arena';
 
 // Constants
 const TWITTER_HANDLE = '_buildspace';
@@ -136,6 +137,9 @@ const App = () => {
        */
     } else if (currentAccount && !characterNFT) {
       return <SelectCharacter setCharacterNFT={setCharacterNFT} />;
+    } else if (currentAccount && characterNFT) {
+      // If there is a connected wallet and characterNFT, it's time to battle!
+      return <Arena characterNFT={characterNFT} />;
     }
   };
 
