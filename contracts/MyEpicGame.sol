7183bc74197c6a19bdf3f9fdb8eafcfc09176cff
onemanatatime <8282076+onemanatatime@users.noreply.github.com>
2022-12-16 13:19:57 +0800

    add utility functions to be used on web app

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -39,6 +39,9 @@ contract MyEpicGame is ERC721 {
   // to store the owner of the NFT and reference it later.
   mapping(address => uint256) public nftHolders;
 
+  event CharacterNFTMinted(address sender, uint256 tokenId, uint256 characterIndex);
+  event AttackComplete(address sender, uint newBossHp, uint newPlayerHp);
+
   // Let’s first just build a basic boss struct and initialize its data, similar to how we did for our characters. 
   struct BigBoss {
     string name;
@@ -127,6 +130,8 @@ contract MyEpicGame is ERC721 {
 
     // Increment the tokenId for the next person that uses it.
     _tokenIds.increment();
+
+    emit CharacterNFTMinted(msg.sender, newItemId, _characterIndex);
   }
 
   function tokenURI(uint256 _tokenId) public view override returns (string memory) {
@@ -201,6 +206,8 @@ contract MyEpicGame is ERC721 {
     // Console for ease.
     console.log("Player attacked boss. New boss hp: %s", bigBoss.hp);
     console.log("Boss attacked player. New player hp: %s\n", player.hp);
+
+    emit AttackComplete(msg.sender, bigBoss.hp, player.hp);
   }
 
   uint randNonce = 0; // this is used to help ensure that the algorithm has different inputs every time
@@ -210,6 +217,28 @@ contract MyEpicGame is ERC721 {
     return uint(keccak256(abi.encodePacked(block.timestamp,          // 'now' has been deprecated, use 'block.timestamp' instead
                                           msg.sender,               // your address
                                           randNonce))) % _modulus;  // modulo using the _modulus argument
+  }
+
+  function checkIfUserHasNFT() public view returns (CharacterAttributes memory) {
+    // Get the tokenId of the user's character NFT
+    uint256 userNftTokenId = nftHolders[msg.sender];
+    // If the user has a tokenId in the map, return their character.
+    if (userNftTokenId > 0) {
+      return nftHolderAttributes[userNftTokenId];
+    }
+    // Else, return an empty character.
+    else {
+      CharacterAttributes memory emptyStruct;
+      return emptyStruct;
+    }
+  }
+
+  function getAllDefaultCharacters() public view returns (CharacterAttributes[] memory) {
+    return defaultCharacters;
+  }
+
+  function getBigBoss() public view returns (BigBoss memory) {
+    return bigBoss;
   }
 
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -14,23 +14,6 @@ const main = async () => {
   );
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
-
-  let txn;
-  // We only have three characters.
-  // an NFT w/ the character at index 1 of our array.
-  txn = await gameContract.mintCharacterNFT(1);
-  await txn.wait();
-  
-  // Get the value of the NFT's URI.
-  let returnedTokenUri = await gameContract.tokenURI(1);
-  console.log("Token URI:", returnedTokenUri);
-  
-  txn = await gameContract.attackBoss();
-  await txn.wait();
-
-  txn = await gameContract.attackBoss();
-  await txn.wait();
-
 };
 
 const runMain = async () => {
