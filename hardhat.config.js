8dd6c8875ed6722fe4020b3f247c518b55eecc27
onemanatatime <8282076+sov3333@users.noreply.github.com>
2022-12-18 09:40:14 +0800

    update hardhat.config (add arbi network)

--- hardhat.config.js
+++ hardhat.config.js
@@ -5,7 +5,12 @@ module.exports = {
   solidity: '0.8.17',
   networks: {
     goerli: {
-      url: process.env.QUICKNODE_API,
+      url: process.env.QUICKNODE_GOERLI_API,
+      accounts: [process.env.PRIVATE_KEY],
+    },
+    arbitrum: {
+      chainId: 42161,
+      url: process.env.ALCHEMY_ARBI_API,
       accounts: [process.env.PRIVATE_KEY],
     },
   },
